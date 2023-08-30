136661ed962e48e539f9c8738f980ad3a97e7d00
minh <minhlai@users.noreply.github.com>
2023-08-30 12:30:00 -0400

    ci: push personnel api image to portal client artifactory folder (#5)
    * push personnel api image to portal client artifactory folder
    
    * add dod CA to connect to C1 Art
    
    * add name to step
    
    * change target repo for c1 push to paass
    
    ---------
    
    Co-authored-by: John Gedeon <john@truss.works>

---
+++ scripts/add-dod-cas.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Import DoD root certificates into linux CA store
+
+
+    # Location of bundle from DISA site
+    bundle=https://dl.dod.cyber.mil/wp-content/uploads/pki-pke/zip/unclass-certificates_pkcs7_DoD.zip
+
+    # Extract the bundle
+    cd /usr/local/share/ca-certificates
+    wget $bundle
+    unzip unclass-certificates_pkcs7_DoD.zip
+
+    # Convert the PKCS#7 bundle into individual PEM files
+    openssl pkcs7 -print_certs -in certificates_pkcs7_v5_12_dod/*_pem.p7b |
+        awk 'BEGIN {c=0} /subject=/ {c++} {print > "cert." c ".pem"}'
+
+    # Rename the files based on the CA name
+    for i in *.pem; do
+        name=$(openssl x509 -noout -subject -in $i |
+               awk -F '(=|= )' '{gsub(/ /, "_", $NF); print $NF}'
+        )
+        mv $i ${name}.crt
+    done
+
+    # update certificate stores
+    update-ca-certificates
+
+    rm unclass-certificates_pkcs7_DoD.zip
